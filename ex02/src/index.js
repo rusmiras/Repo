19edb4bf79d33cb228a89db80dfbcb62ffab8569
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-03-30 12:30:31 +0200

    Commenting the code

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    // This is one line comment
+    /* This is multiple
+        line comment */
+    // Only change code above this line
+    
+}
+
+console.log(main());
+
+module.exports = main;
